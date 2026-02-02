db1cc54dfc287a0e4ada9b0428054e3fd121fb00
subhodeep <subhodeeppal64@gmail.com>
2026-02-02 12:41:11 +0000

    volume mount path fixed

--- dags/random_delta_dag.py
+++ dags/random_delta_dag.py
@@ -20,11 +20,25 @@ with DAG(
     template_searchpath=[os.path.dirname(__file__)],
 ) as dag:
 
+    # Dynamically detect the Python file in the PVC
+    dags_repo = '/opt/airflow/dags/repo/scripts'
+    script_file = None
+    for f in os.listdir(dags_repo):
+        if f.startswith("random_delta_gen") and f.endswith(".py"):
+            script_file = f
+            break
+
+    if not script_file:
+        raise FileNotFoundError(f"No script found in {dags_repo}")
+
+    full_path = f"local:///{dags_repo}/{script_file}"
+
     submit_job = SparkKubernetesOperator(
         task_id='submit_random_delta_job',
         namespace='default',
         application_file="random_delta_manifest.yaml",
         params={
+            'main_application_file': full_path,
             's3_endpoint': 'http://minio.default.svc.cluster.local:9000'
         }
     )
