9eaff26fa4d3f447aa0a502fc61b480dcec477dc
subhodeep <subhodeeppal64@gmail.com>
2026-02-17 11:55:36 +0000

    New dyanmic app_name added in nav raw data ingestion step

--- dags/spark_nav_raw_ingestion_v2.py
+++ dags/spark_nav_raw_ingestion_v2.py
@@ -29,7 +29,7 @@ with DAG(
         application_file="spark_nav_raw_ingestion_manifest.yaml",
         do_xcom_push=True,
         params={
-            's3_endpoint': 'http://minio.default.svc.cluster.local:9000'
+            's3_endpoint': 'http://minio.default.svc.cluster.local:9000',
             'app_name': "spark-nav-data-raw-{{ ts_nodash | lower }}"
         }
     )
